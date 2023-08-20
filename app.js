2d254c65b46fb2995641571d270be9b04fbabbbd
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-08-20 20:22:21 +0300

    project 3

---
+++ app.js
@@ -0,0 +1,40 @@
+const plus1 = document.querySelector('.plus1');
+
+plus1.addEventListener('click', function () {
+    const res = document.querySelector('.response1');
+    if (res.hidden === true) {
+        res.hidden = false;
+    } else {
+        res.hidden = true;
+    }
+})
+const plus2 = document.querySelector('.plus2');
+
+plus2.addEventListener('click', function () {
+    const res = document.querySelector('.response2');
+    if (res.hidden === true) {
+        res.hidden = false;
+    } else {
+        res.hidden = true;
+    }
+})
+const plus3 = document.querySelector('.plus3');
+
+plus3.addEventListener('click', function () {
+    const res = document.querySelector('.response3');
+    if (res.hidden === true) {
+        res.hidden = false;
+    } else {
+        res.hidden = true;
+    }
+})
+const plus4 = document.querySelector('.plus4');
+
+plus4.addEventListener('click', function () {
+    const res = document.querySelector('.response4');
+    if (res.hidden === true) {
+        res.hidden = false;
+    } else {
+        res.hidden = true;
+    }
+})
